4e113959a3ca8b35c6ba694abf7715f5833ec90f
Peter Kaagman <prjv.kaagman@gmail.com>
2023-07-01 12:37:29 +0200

    roles

--- lib/etm.pm
+++ lib/etm.pm
@@ -9,73 +9,97 @@ use JSON::Create 'create_json';
 our $VERSION = '0.1';
 
 get '/' => sub { # {{{1
+  if (validateLogin()){
+    say "Valid login";
+    my $sessionData = session->read('oauth');
+    my $teams = session->read('teams');
+    template 'index' => { 'title' => 'ETM',
+  	                'sessionData' => $sessionData, 
+  			'joinedTeams' => $teams,
+  			};
+  }else{
+    say "inValid login";
+    redirect '/about';
+  }
+};# }}}
+get '/about' => sub { # {{{1
   my $sessionData = session->read('oauth');
   #my $teams = getJoinedTeams($$sessionData{azuread}{access_token});
   my $teams = session->read('teams');
-  template 'index' => { 'title' => 'ETM',
+  print "/n/nAbout/n/n";
+  template 'about' => { 'title' => 'ETM About',
 	                'sessionData' => $sessionData, 
-			'joinedTeams' => $teams,
 			};
 };# }}}
-
 post '/api/sendmessage' => sub { # {{{1
-  my $data = from_json(request->body);
+  if (validateLogin()){
+    my $data = from_json(request->body);
 
-  # Bereid een JSON object voor met content
-  my %content;
-  $content{'body'}{'content'} = $$data{'message'};
-  print create_json(\%content);
-
+    # Bereid een JSON object voor met content
+    my %content;
+    $content{'body'}{'content'} = $$data{'message'};
+    print create_json(\%content);
+  }
 }; #}}}
-
 get '/api/reloadteams' => sub { # {{{1
-  session->delete('teams');
-  return redirect "/";
+  if (validateLogin()){
+    session->delete('teams');
+    loadTeams();
+    my $teams_data = session->read('teams');
+    send_as JSON => $teams_data;
+  }
 }; #}}}
-sub _doGetJoinedTeams { # {{{1
+sub getJoinedTeams { # {{{1
+  my $token = shift;
+  my @teams;
+  my $url = "https://graph.microsoft.com/v1.0/me/joinedTeams";
+  _doGetItems($token, $url, \@teams);
+  #print Dumper \@teams;
+  #return \@teams;
+  my %teamsData;
+  foreach my $team (@teams){
+	$teamsData{teams}{$$team{id}}{displayName} = $$team{displayName};
+	$teamsData{teams}{$$team{id}}{description} = $$team{description};
+
+	# Team staat in de hash, mooi moment om aanvullende
+	# gegevens over het team te zoeken
+	my @channels;
+	getTeamChannels($token, $$team{id},\@channels);
+	foreach my $channel (@channels){
+	  $teamsData{teams}{$$team{id}}{channels}{$$channel{id}}{displayName} = $$channel{displayName};
+	  $teamsData{teams}{$$team{id}}{channels}{$$channel{id}}{description} = $$channel{description};
+	}
+  }
+  session->write(%teamsData);
+}# }}}
+sub getTeamChannels { #{{{1
+  my $token = shift;
+  my $teamID = shift;
+  my $channels = shift;
+  say "Getting channels for ID $teamID";
+  my $url = "https://graph.microsoft.com/v1.0/teams/$teamID/channels";
+  _doGetItems($token, $url, $channels);
+}#}}}
+# _doGetItem: Recursive functions to get items from graph {{{1
+sub _doGetItems {
 	my $token = shift;
 	my $url = shift;
-	my $teams = shift;
+	my $items = shift;
 	my $result = _callAPI($token,$url, 'GET');
 	if ($result->is_success){
 		my $reply =  decode_json($result->decoded_content);
 		while (my ($i, $el) = each @{$$reply{'value'}}) {
-			push @{$teams}, $el;
+			push @{$items}, $el;
 		}
 		if ($$reply{'@odata.nextLink'}){
-			do_fetch($token,$$reply{'@odata.nextLink'}, $teams);
+			_doGetItems($token,$$reply{'@odata.nextLink'}, $items);
 		}
 		#print Dumper $$reply{'value'};
 	}else{
 	#	print Dumper $result;
 		die $result->status_line;
 	}
-} #	}}}
-
-sub getJoinedTeams { # {{{1
-  my $token = shift;
-  my @teams;
-  my $url = "http://graph.microsoft.com/v1.0/me/joinedTeams";
-  _doGetJoinedTeams($token, $url, \@teams);
-  #print Dumper \@teams;
-  #return \@teams;
-  my %teamsData;
-  foreach(@teams){
-	$teamsData{teams}{$$_{id}}{displayName} = $$_{displayName};
-	$teamsData{teams}{$$_{id}}{description} = $$_{description};
-
-	# Team staat in de hash, mooi moment om aanvullende
-	# gegevens over het team te zoeken
-	getTeamChannels($token, %teamData{teams}{id});
-  }
-  session->write(%teamsData);
-}# }}}
-
-sub getTeamChannels {
-  my $token = shift;
-  my $teamID = shift;
-}
-
+} #}}}
 sub loadTeams { #{{{1
     my $teams_data = session->read('teams');
     my $session_data = session->read('oauth');
@@ -86,37 +110,55 @@ sub loadTeams { #{{{1
          (!defined $teams_data) &&
 	 (defined $session_data->{$provider}{id_token})
 	){
-  		#print "\nLoading teams\n\n";
   		getJoinedTeams($$session_data{azuread}{access_token});
      }
 }#}}}
-
-hook before => sub {# {{{1
-    my $session_data = session->read('oauth');#{{{2
-    my $provider = "azuread"; # Lower case of the authentication plugin used
+# sub validateLogin() {{{1
+# Controleert:
+# - of er een sessie
+# - of de sessie ververst moet worden
+# - of de ingelogde gebruiker de juiste rollen heeft
+sub validateLogin { 
+  say "validateLogin";
+  my $session_data = session->read('oauth');
+  my $provider = "azuread"; # Lower case of the authentication plugin used
  
-    my $now = DateTime->now->epoch;
+  my $now = DateTime->now->epoch;
+
+  if ((
+      !defined $session_data || 
+      !defined $session_data->{$provider} || 
+      !defined $session_data->{$provider}{id_token}
+      ) && request->path !~ m{^/auth}) {
+    return forward "/auth/$provider";
  
-    if ((
-	!defined $session_data || 
-	!defined $session_data->{$provider} || 
-	!defined $session_data->{$provider}{id_token}
-	) && request->path !~ m{^/auth}) {
-      return forward "/auth/$provider";
+  } elsif ( 
+          defined $session_data->{$provider}{refresh_token} && 
+          defined $session_data->{$provider}{expires} && 
+          $session_data->{$provider}{expires} < $now && request->path !~ m{^/auth}
+  ) {
+    return forward "/auth/$provider/refresh";
+  }
+
  
-    } elsif ( 
-	    defined $session_data->{$provider}{refresh_token} && 
-	    defined $session_data->{$provider}{expires} && 
-	    $session_data->{$provider}{expires} < $now && request->path !~ m{^/auth}
-    ) {
-      return forward "/auth/$provider/refresh";
-    }# }}}
+  say "Roles:";
+  #say  $$session_data{azuread}{login_info}{roles}[0];
+  if (!defined $session_data->{$provider}{login_info}{roles}){
+    say "geen roles";
+    return 0;
+  }
 
-    loadTeams();
-
-
-};# }}}
-
+  if (
+       ('etm' ~~ $session_data->{$provider}{login_info}{roles}) &&
+       ('Medewerkers' ~~ $session_data->{$provider}{login_info}{roles})
+     ){
+    say "role etm en Medewerkers";
+    return 42;
+  }else{
+    say "geen role etm en Medewerkers";
+    return 0;
+  }
+}#}}}
 sub _callAPI { # {{{1
 	my $token = shift;
 	my $url = shift;
@@ -128,9 +170,8 @@ sub _callAPI { # {{{1
 	my $req = HTTP::Request->new();
 	$req->method($verb);
 	$req->uri($url);
-	$req->header('Accept' => '*/*');
 	$req->header('Accept'        => '*/*',            );
-	$req->header('Authorization' => "Bearer ".$token, );
+	$req->header('Authorization' => "Bearer $token", );
 	$req->header('User-Agent'    => 'curl/7.55.1',    );
 	$req->header('Content-Type'  => 'application/json');
 	if (defined $content){
@@ -141,4 +182,4 @@ sub _callAPI { # {{{1
 	return $result;
 } # }}}
 
-true;
+42;
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,29 +1,14 @@
-// jQuery
-//$(function() {
-//
-//  console.log("Document ready");
-//
-//  $("#sendMessage").on( "submit", function(event){
-//    event.preventDefault();
-//    if ( $("#message").val().length > 0 ) {
-//    console.log("submitted send form");
-//    $("input:checked").each(function (index){
-//      console.log("team: "+index+" this: "+this.id);
-//    });
-//    }else{
-//      console.log("Bericht is leeg");
-//    }
-//  })
-//})
-
 // vanilla JS
 document.addEventListener( 'DOMContentLoaded', function() {
   console.log("Document ready");
 
+  loadTeams();
+
   let reloadTeams = document.querySelector('#reloadTeams');
   reloadTeams.addEventListener('click', function(event){
     event.preventDefault();
-    fetch('/api/reloadteams')
+    loadTeams();
+    //fetch('/api/reloadteams');
   });
 
   let form = document.querySelector('#sendMessage');
@@ -54,5 +39,26 @@ document.addEventListener( 'DOMContentLoaded', function() {
       console.log('Bericht is leeg');
     }
   });
+
+  function loadTeams(){
+    console.log('loadTeams');
+    fetch('/api/reloadteams')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      var table = '<table>';
+      for (let id in data){
+        table += '<tr><td>'+id+'</td></tr>';
+      }
+      table += '</table>';
+      var tc = document.querySelector('#teamList');
+      tc.innerHTML = table;
+    })
+    .catch(function(err){
+      console.log('error');
+      console.warn('Some error: ', err);
+    });
+  }
 
 }, false);
