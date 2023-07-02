63c6671ee8198cccb8435f3b21dd4301cdefa9dc
Peter Kaagman <prjv.kaagman@gmail.com>
2023-07-02 23:46:49 +0200

    JS refresh cleanup

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -46,14 +46,54 @@ document.addEventListener( 'DOMContentLoaded', function() {
     .then(function(response){
       return response.json();
     })
-    .then(function(data){
-      var table = '<table>';
-      for (let id in data){
-        table += '<tr><td>'+id+'</td></tr>';
-      }
-      table += '</table>';
-      var tc = document.querySelector('#teamList');
-      tc.innerHTML = table;
+    .then(function(teamsObj){
+      const list = document.querySelector('#teamList');
+      const table = document.createElement(`table`);
+      const aRow = document.createElement(`tr`);
+      const aCell = document.createElement(`td`);
+      const aHead = document.createElement(`th`);
+      
+      // Headers
+      const row = aRow.cloneNode();
+      const cellNaam = aHead.cloneNode();
+      cellNaam.textContent = 'Naam';
+      row.append(cellNaam);
+      const cellRol = aHead.cloneNode();
+      cellRol.textContent = 'Rol';
+      row.append(cellRol);
+      const cellMessage = aHead.cloneNode();
+      cellMessage.textContent = 'Bericht';
+      row.append(cellMessage);
+      table.append(row);
+
+      // Data rows
+      const rows = Object.entries(teamsObj).forEach( ([id, value]) => {
+	// Start row
+        const row = aRow.cloneNode();
+	// Naam
+        const cellNaam = aCell.cloneNode();
+	const anchor = document.createElement('a');
+	anchor.href = `/teamdetail/${id}`;
+	anchor.innerText = teamsObj[id].displayName;
+	cellNaam.append(anchor);
+	row.append(cellNaam);
+	// Rol
+        const cellRol = aCell.cloneNode();
+	cellRol.textContent = 'ntb'
+	row.append(cellRol);
+	// Bericht
+        const cellMessage = aCell.cloneNode();
+	const checkbox = document.createElement('input');
+	checkbox.setAttribute('type', 'checkbox');
+	checkbox.setAttribute('id', id);
+	checkbox.setAttribute('name', id);
+	cellMessage.append(checkbox);
+	row.append(cellMessage);
+	// End row
+	table.append(row);
+      });
+      list.textContent = ``;
+      list.append(table);
     })
     .catch(function(err){
       console.log('error');
